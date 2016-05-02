6d41476f7c3a384f928cbb4cf05291945dc77e4d
FunFunCoding <method1119@gmail.com>
2016-05-03 02:14:33 +0900

    문제 해답

--- Java_Practice/src/Practice/plus.java
+++ Java_Practice/src/Practice/plus.java
@@ -9,6 +9,20 @@
 첫째 줄에 A+B를 출력한다.*/
 package Practice;
 
+import java.util.Scanner;
+
 public class plus {
-	
+	public static void main(String[] args) {
+		int a,b=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자 A를 입력해 주세요");
+		a = sc.nextInt();
+		System.out.print("\n숫자 B를 입력해 주세요");
+		b = sc.nextInt();
+		if(a > 0 && a < 10 && b > 0 && b < 10){
+			System.out.println("\n입력하신 " + a + " + " + b + "는 " + (a+b) + "입니다.");
+		}else{
+			System.out.println("\n A와 B는 1~9사이의 숫자를 입력해 주세요");
+		}
+	}
 }
